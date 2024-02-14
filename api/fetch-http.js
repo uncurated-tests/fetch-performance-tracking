54d101e07916ccba923d89380bb0b410ec0b487e
Ethan Arrowood <ethan@arrowood.dev>
2024-02-14 12:18:05 -0700

    .get is fake news

--- api/fetch-http.js
+++ api/fetch-http.js
@@ -12,14 +12,11 @@ let temp = 'cold';
  * @param {import('node:http').OutgoingMessage} res 
  */
 export default async function handler(req, res) {
-    console.log(req.headers);
-    console.log('authorization', req.headers.get('authorization'));
-
     const context = {
         host: req.headers.host,
         endpoint: new URL('http://example.com'),
         temp,
-        isCron: isCron(req.headers.get('authorization'))
+        isCron: isCron(req.headers.authorization)
     }
 
     const durations = await processRequests(context);
